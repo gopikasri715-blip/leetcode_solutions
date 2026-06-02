8b8cbc61fe174d2b40fb367967eefdc2d8dc6e07
gopikasri715-blip <gopikasri715@gmail.com>
2026-06-02 15:46:53 +0530

    solve: Squares of a Sorted Array (#977) - Two Pointers O(n) | beats 36.77%

---
+++ arrays/977_squares_of_a_sorted_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortedSquares(self, nums):
+        result = [0]*len(nums)
+        left = 0
+        right = len(nums)-1
+        pos= len(nums)-1
+        while left<=right:
+            if abs(nums[left])<abs(nums[right]):
+                result[pos] = nums[right]*nums[right]
+                right-=1
+            else:
+                result[pos]= nums[left]*nums[left]
+                left+=1
+            pos-=1
+        return result
