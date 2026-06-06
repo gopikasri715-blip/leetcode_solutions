920143a6a2b072f12e579a6c06da7ccf64a86796
gopikasri715-blip <gopikasri715@gmail.com>
2026-06-06 15:52:47 +0530

    solve: Permutation in String (#567) - Sliding Window O(n) | beats 22.18%

---
+++ sliding_window/567_permutation_in_string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkInclusion(self,s1,s2):
+        s1_count = Counter(s1)
+        window = Counter()
+
+        for i in range(len(s2)):
+            window[s2[i]] += 1
+
+            if i >= len(s1):
+                left_char = s2[i - len(s1)]
+                window[left_char] -= 1
+
+                if window[left_char] == 0:
+                    del window[left_char]
+
+            if window == s1_count:
+                return True
+
+        return False
+        
