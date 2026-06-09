fb8173a6d985f11dd8044a2441b02735891b7526
gopikasri715-blip <gopikasri715@gmail.com>
2026-06-09 21:29:35 +0530

    solve: Excel Sheet Column Title (#168) - Math O(log n) | beats 12.26%

---
+++ math/168_excel_sheet_column_title.py
@@ -0,0 +1,12 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        result = ""
+
+        while columnNumber > 0:
+            columnNumber -= 1
+
+            result += chr(columnNumber % 26 + ord('A'))
+
+            columnNumber //= 26
+
+        return result[::-1]
