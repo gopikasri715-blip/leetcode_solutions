d6c0684386dc29bf92ba24d644c247a9a8150c16
gopikasri715-blip <gopikasri715@gmail.com>
2026-06-11 18:57:56 +0530

    solve: Longest Harmonious Subsequence (#594) - Hash Map O(n) | beats 66.70%

---
+++ hashing/594_longest_harmonious_subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findLHS(self, nums):
+        count = Counter(nums)
+
+        longest = 0
+
+        for num in count:
+            if num + 1 in count:
+                longest = max(
+                    longest,
+                    count[num] + count[num + 1]
+                )
+
+        return longest
