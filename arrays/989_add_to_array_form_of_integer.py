62ff942a9c69edf0bd469579e5d91f0a92e46035
gopikasri715-blip <gopikasri715@gmail.com>
2026-06-14 19:54:03 +0530

    solve: Add to Array-Form of Integer (#989) - Array Math O(n) | beats 83.30%

---
+++ arrays/989_add_to_array_form_of_integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addToArrayForm(self, num, k):
+        i = len(num) - 1
+
+        while i >= 0 and k > 0:
+            k += num[i]
+
+            num[i] = k % 10
+            k //= 10
+
+            i -= 1
+
+        while k > 0:
+            num.insert(0, k % 10)
+            k //= 10
+
+        return num
