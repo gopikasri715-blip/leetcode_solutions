fe6d6153a37b1ef10153829692021d7355031a79
gopikasri715-blip <gopikasri715@gmail.com>
2026-06-18 12:03:06 +0530

    solve: Decompress Run-Length Encoded List (#1313) | 7.29%

---
+++ arrays/1313_decompress_run_length_encoded_list.py
@@ -0,0 +1,11 @@
+class Solution:
+    def decompressRLElist(self, nums):
+        result = []
+
+        for i in range(0, len(nums), 2):
+            freq = nums[i]
+            val = nums[i+1]
+
+            result.extend([val] * freq)
+
+        return result
