52cedbf13a2638d12270c3f0c57b39c7fca3a525
gopikasri715-blip <gopikasri715@gmail.com>
2026-06-21 19:46:10 +0530

    solve: Number of Students Doing Homework (#1450)|beats 100.00%

---
+++ arrays/1450_students_doing_homework.py
@@ -0,0 +1,9 @@
+class Solution:
+    def busyStudent(self, startTime, endTime, queryTime):
+        count = 0
+
+        for i in range(len(startTime)):
+            if startTime[i] <= queryTime <= endTime[i]:
+                count += 1
+
+        return count
